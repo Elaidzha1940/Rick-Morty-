d13b0902bdf931d4b8d0f286136d8d229805f1c0
Elaidzha1940 <eli080293@gmail.com>
2023-10-02 21:37:53 +0300

    Rick&Morty + Done

--- Rick&Morty/ContentView.swift
+++ Rick&Morty/ContentView.swift
@@ -8,14 +8,48 @@
 //  */
 
 import SwiftUI
+import Alamofire
+
+struct Character: Identifiable, Decodable {
+    let id: Int
+    let name: String
+    let status: String
+}
+
+struct CharacterResponse: Decodable {
+    let results: [Character]
+}
 
 struct ContentView: View {
+    @State private var characters: [Character] = []
+
     var body: some View {
-        
-        VStack {
-          
-            
+        NavigationView {
+            List(characters) { character in
+                VStack(alignment: .leading) {
+                    Text(character.name)
+                        .font(.headline)
+                    Text(character.status)
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+            }
+            .navigationTitle("Rick and Morty Characters")
+            .onAppear(perform: loadData)
         }
+    }
+
+    func loadData() {
+        AF.request("https://rickandmortyapi.com/api/character")
+            .validate()
+            .responseDecodable(of: CharacterResponse.self) { response in
+                switch response.result {
+                case .success(let characterResponse):
+                    self.characters = characterResponse.results
+                case .failure(let error):
+                    print("Error loading data: \(error)")
+                }
+            }
     }
 }
 
