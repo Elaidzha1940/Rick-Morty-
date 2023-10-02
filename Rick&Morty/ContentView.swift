3585c169962624471db7be012c877df6ad1f0768
Elaidzha1940 <eli080293@gmail.com>
2023-10-02 21:26:45 +0300

    Rick&Morty

---
+++ Rick&Morty/ContentView.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: Rick&Morty
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 02.10.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+          
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: Rick&Morty
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 02.10.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
